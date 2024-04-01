0f2b5efef8beb312d973638654e1959a3243d52d
Akshat Gupta <guptaakshat370@gmai.com>
2024-04-01 16:44:24 +0530

    feat: Configured Progressive Web App (PWA) setup
    -Installed necessary dependencies for PWA functionality
    
    Configured manifest file for app installation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+//     dest: 'public'
+// })
+  
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default withPWA(nextConfig);
+import withPWA from 'next-pwa';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
+
+
